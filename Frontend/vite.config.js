4f9ce72f7c13901ee7daf594b16050cac8ddd241
Shashwat Gupta <Shashwat_Gupta@epam.com>
2025-07-10 12:37:48 +0530

    restored vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,23 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy:{
-      '/api' : {
-        target: 'http://localhost:5000',
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    outDir: 'dist', // default value
-  }
-})
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+			},
+		},
+	},
+});
